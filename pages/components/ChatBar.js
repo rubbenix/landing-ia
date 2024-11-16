e65e50d9af4d2c63073282ad502a3a8343c7b3cb
rubbenix <rubenix02@gmail.com>
2024-11-16 16:15:02 +0100

    Updating ChatBar, more clean

--- pages/components/ChatBar.js
+++ pages/components/ChatBar.js
@@ -1,12 +1,9 @@
 import { TextInput, Button, Group } from '@mantine/core';
 import { IconSend } from '@tabler/icons-react';
 
-
 function ChatBar() {
   return (
-    <div
-      className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-4xl p-4 z-50"
-    >
+    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-4xl p-4 z-50">
       <Group
         position="apart"
         className="bg-white rounded-full p-3 shadow-lg border border-gray-200"
@@ -17,17 +14,25 @@ function ChatBar() {
           radius="xl"
           size="md"
           className="flex-grow"
+          styles={{
+            input: { width: '100%', border: 'none', outline: 'none', padding: '0' },
+          }}
         />
 
-        {/* Botón de enviar */}
+        {/* Botón de enviar con el icono alineado a la derecha */}
         <Button
           radius="xl"
           size="md"
           color="blue"
-          className="text-white flex items-center justify-center"
-        >
-          <IconSend size={0} />
-        </Button>
+          rightIcon={<IconSend size={20} />}
+          styles={{
+            root: {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+          }}
+        />
       </Group>
     </div>
   );
